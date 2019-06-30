e3689e42ccada37a3388563939eec9745851aeac
Gustavo <Gustavo@g.coms>
2019-06-29 22:58:52 -0400

    database

--- config/database.php
+++ config/database.php
@@ -7,7 +7,7 @@ $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $host = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
-$database = substr($url["path"], 1);cle
+$database = substr($url["path"], 1);
 
 return [
 
