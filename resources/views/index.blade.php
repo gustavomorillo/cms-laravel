e4a832a4791f103a9357521056ccf35420c3ac4c
Gustavo <Gustavo@g.coms>
2019-06-29 23:18:11 -0400

    styles

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,7 +12,7 @@
     <title>Blog Home - Start Bootstrap Template</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="{{ asset('css/all.css') }}" rel="stylesheet">
+    <link href="css/all.css" rel="stylesheet">
 
     <!-- Custom CSS -->
     <link href="css/blog-home.css" rel="stylesheet">
