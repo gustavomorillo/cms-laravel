994f59018e8ed5a8eb79c11d68d05ac0090fed2e
Gustavo <Gustavo@g.coms>
2019-06-30 00:11:34 -0400

    request problem3

--- app/Http/Controllers/AdminCategoriesController.php
+++ app/Http/Controllers/AdminCategoriesController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\http\Requests\CreateCategoryRequest;
+use App\http\requests\CreateCategoryRequest;
 use Illuminate\Support\Facades\Session;
 use App\Category;
 
